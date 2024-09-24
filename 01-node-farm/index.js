145ae7f707e968861474abe85b60bc1bb3a60429
Ahmed Hany <ah7608867@gmail.com>
2024-09-24 16:08:14 +0300

    servers

--- 01-node-farm/index.js
+++ 01-node-farm/index.js
@@ -1,14 +1,14 @@
 const file_system = require('fs');
+const http = require('http');
+const url = require('url');
 
-
-
-// 1) File System Module
+// 1) File System 
 
 // 1.1) Reading Files
 
 /*
 to read a file we use the fs.readFile() Asynchronously and fs.readFileSync() Synchronously.
-fs.readFile() takes 3 arguments: 
+fs.readFile() takes 3 arguments:
 1. path of the file
 2. encoding type
 3. callback function that takes 2 arguments: error and data
@@ -22,7 +22,7 @@ fs.readFileSync() takes 2 arguments:
 // 1.1.1) file_system.readFile()
 // this is not recommended because it blocks the code execution.
 // don't return the data, instead pass it to a callback function
-
+/*
 const sync_callback = function (error, data) {
     try {
         if (error) throw error;
@@ -42,7 +42,7 @@ file_system.readFile('./txt/input.txt', 'utf-8', sync_callback);
 
 const async_file = file_system.readFileSync('./txt/input.txt', 'utf-8');
 console.log('Synchronously read :', async_file);
-
+*/
 // 1.2) Writing Files
 /*
 to write a file we use the fs.writeFile() Asynchronously and fs.writeFileSync() Synchronously.
@@ -57,7 +57,7 @@ fs.writeFileSync() takes 2 arguments:
 2. data to write
 */
 
-
+/*
 const text_to_write = `this is the file i read before 1 seconde\n${async_file}`;
 const path_url = "./txt/output.txt";
 // 1.2.1) file_system.writeFile()
@@ -80,3 +80,60 @@ file_system.writeFile(path_url, text_to_write, (error) => {
 file_system.writeFileSync(path_url, text_to_write);
 
 console.log('Synchronously: file written successfully');
+
+*/
+
+
+// 2) Servers
+
+// 2.1) Creating a server
+
+/*
+to create a server we use the http.createServer() method that takes a callback function that takes 2 arguments: request and response.
+*/
+
+/*
+Routes: is the part of the URL that determines which page to show.
+for example: in the URL http://localhost:3000/home, the route is /home
+the server should respond to different routes with different responses.
+to get the route we use the request.url property.
+*/
+
+/*
+Write headers: the server should write headers before writing the response.
+we use it to set the content type of the response, for example, text/html, text/plain, application/json, etc. and the status code of the response, for example, 200, 404, 500, etc.
+*/
+
+const serverRequest = function (request, response) {
+    // get the route
+    const route = request.url;
+
+    if (route === '/') {
+        response.end('Welcome to the home page');
+    }
+    else if (route === "/nudes") {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end('we learn node.js here, no nudes');
+    }
+    else if (route === "/haterror") {
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        response.end('<h1>404 page not found</h1>');
+    }
+    else {
+        response.end('Hello from Hany');
+    }
+};
+
+const server = http.createServer(serverRequest)
+
+// 2.2) Starting a server
+
+/*
+to start a server we use the server.listen() method that takes 2 arguments: port and host.
+port is the port number that the server will listen to.
+host is the host name or IP address of the server.
+*/
+
+server.listen(3000, "127.0.0", () => {
+    console.log('server is started');
+});
