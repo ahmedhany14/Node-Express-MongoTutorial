65138ce98f6e4fc45a7f77d9c09d4ae2609cc880
Ahmed Hany <ah7608867@gmail.com>
2024-10-06 20:03:44 +0300

    Refactoring Code v1

--- 03-Express/01-Express_mongoDB/app.js
+++ 03-Express/01-Express_mongoDB/app.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const file_system = require('fs');
-
 const app = express();
-
-// middleware
-app.use(express.json());
-
-// tours api
-
-
-// get all tours
+app.use(express.json()); // middleware
 const tours = JSON.parse(file_system.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-app.get('/api/v1/tours', (request, responce) => {
+
+
+const GetAllTouts = (request, responce) => {
 
     responce.status(200).json({
         status: 'success',
@@ -20,15 +14,9 @@ app.get('/api/v1/tours', (request, responce) => {
             tours: tours
         }
     })
-});
+};
 
-// get a specific tour by id
-/*
-to make a parameter in the URL, we use : before the parameter name
-to make the parameter optional, we use ? after the parameter name
-to get the parameter value, we use request.params and from it we can get the parameter value
-*/
-app.get('/api/v1/tours/:id', (request, responce) => {
+const GetTour = (request, responce) => {
     console.log(request.params);
     let id = parseInt(request.params.id);
     const tour = tours.find((element) => {
@@ -49,11 +37,9 @@ app.get('/api/v1/tours/:id', (request, responce) => {
             }
         });
     }
-});
+}
 
-
-// Post a new tour
-app.post('/api/v1/tours', (request, responce) => {
+const CreateTour = (request, responce) => {
     // to get the data user sent, we use request.body
     const new_id = tours[tours.length - 1].id + 1;
     const new_tour = Object.assign({ id: new_id }, request.body);
@@ -76,11 +62,9 @@ app.post('/api/v1/tours', (request, responce) => {
         }
     });
     //responce.send('Done');
-});
+}
 
-
-// Update a tour, by using patch method
-app.patch('/api/v1/tours/:id', (request, responce) => {
+const UpdateTour = (request, responce) => {
     let id = parseInt(request.params.id);
     let tour = tours.find((element) => {
         return element.id === id;
@@ -117,11 +101,9 @@ app.patch('/api/v1/tours/:id', (request, responce) => {
             })
         }
     });
-});
+}
 
-// Delete a tour
-
-app.delete('/api/v1/tours/:id', (request, responce) => {
+const DeleteTour = (request, responce) => {
     let id = parseInt(request.params.id);
     let tour = tours.find((element) => {
         return element.id === id;
@@ -150,43 +132,36 @@ app.delete('/api/v1/tours/:id', (request, responce) => {
             })
         }
     });
+}
 
-});
+
+
+// get a specific tour by id
+/*
+to make a parameter in the URL, we use : before the parameter name
+to make the parameter optional, we use ? after the parameter name
+to get the parameter value, we use request.params and from it we can get the parameter value
+*/
+/*
+app.get('/api/v1/tours', GetAllTouts);
+app.get('/api/v1/tours/:id', GetTour);
+app.post('/api/v1/tours', CreateTour); // Create a new tour
+app.patch('/api/v1/tours/:id', UpdateTour); // Update a tour
+app.delete('/api/v1/tours/:id', DeleteTour); // Delete a tour
+*/
+
+
+app.route('/api/v1/tours')
+    .get(GetAllTouts)
+    .post(CreateTour);
+app.route('/api/v1/tours/:id')
+    .get(GetTour)
+    .patch(UpdateTour)
+    .delete(DeleteTour);
+
 // start express app
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`You are listening to port ${PORT}`);
     console.log('api url: 127.0.0.1:3000')
 });
-
-
-// Create a route response for the root URL
-/*
-get is a method that used to send a response to a get request
-*/
-
-/*
-app.get('/', (request, responce) => {
-
-    // to send a message response
-    //responce.send("Hany tells you to go to /about");
-
-    // to send a JSON response
-    responce.json({
-        name: "Hany",
-        age: 22,
-        location: "EGY"
-    });
-});
-*/
-
-
-/*
-post is a method that used to send a response to a post request
-it is different from get method, in post method you can send a body with the request
-*/
-/*
-app.post('/', (request, responce) => {
-    responce.send('You can post to this URL');
-})
-*/
