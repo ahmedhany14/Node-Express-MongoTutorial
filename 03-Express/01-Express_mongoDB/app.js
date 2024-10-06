8f9d6c78eab363e64d7cf2e963d1a7452a2be642
Ahmed Hany <ah7608867@gmail.com>
2024-10-06 19:41:41 +0300

    Modify a tour api | PATCH HTTP

--- 03-Express/01-Express_mongoDB/app.js
+++ 03-Express/01-Express_mongoDB/app.js
@@ -23,11 +23,14 @@ app.get('/api/v1/tours', (request, responce) => {
 });
 
 // get a specific tour by id
-
+/*
+to make a parameter in the URL, we use : before the parameter name
+to make the parameter optional, we use ? after the parameter name
+to get the parameter value, we use request.params and from it we can get the parameter value
+*/
 app.get('/api/v1/tours/:id', (request, responce) => {
     console.log(request.params);
-    let id = request.params.id;
-    id = parseInt(id.slice(1));
+    let id = parseInt(request.params.id);
     const tour = tours.find((element) => {
         return element.id === id;
     });
@@ -75,6 +78,46 @@ app.post('/api/v1/tours', (request, responce) => {
     //responce.send('Done');
 });
 
+
+// Update a tour, by using patch method
+app.patch('/api/v1/tours/:id', (request, responce) => {
+    let id = parseInt(request.params.id);
+    let tour = tours.find((element) => {
+        return element.id === id;
+    });
+
+    if (!tour) {
+        responce.status(404).json({
+            succes: "faild",
+            message: "Invalid ID"
+        })
+        return;
+    }
+    const new_data = request.body;
+
+
+    for (let key in new_data) {
+        console.log(typeof key)
+        tours[id][key] = new_data[key]
+    }
+    
+    file_system.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        if (err) {
+            responce.status(404).json({
+                status: 'fail',
+                message: 'Error in writing the file'
+            });
+        }
+        else {
+            responce.status(201).json({
+                status: 'success modified',
+                data: {
+                    tour: tours[id]
+                }
+            })
+        }
+    });
+});
 
 // start express app
 const PORT = 3000;
