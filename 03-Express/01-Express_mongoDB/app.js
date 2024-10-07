82ec9f7b5e8b7b38e6a6643d4caf04c2fbc2cf02
Ahmed Hany <ah7608867@gmail.com>
2024-10-07 22:08:42 +0300

    middleware

--- 03-Express/01-Express_mongoDB/app.js
+++ 03-Express/01-Express_mongoDB/app.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const file_system = require('fs');
 const app = express();
-app.use(express.json()); // middleware
+const morgan = require('morgan');
+
 const tours = JSON.parse(file_system.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+
+app.use(express.json()); // middleware
+
+
+// custom middleware
+
+app.use((request, responce, next) => {
+    console.log('Hello from the  first middleware function\n');
+    // to continue to the next middleware, we use next()
+    next();
+})
+
+app.use((request, responce, next) => {
+
+    console.log('You are in the second middleware function');
+
+    //  log host name, request method, request url
+    console.log(`Host: ${request.hostname}`);
+    console.log(`method: ${request.method}`);
+    console.log(`url: ${request.path}`);
+
+    // to continue to the next middleware, we use next()
+    next();
+});
+
+app.use((request, responce, next) => {
+    request.reqTime = new Date().toISOString();
+    console.log(`request time: ${request.reqTime}`);
+    next();
+})
+
+// third party middleware
+app.use(morgan('dev'));
 
 
 const GetAllTouts = (request, responce) => {
