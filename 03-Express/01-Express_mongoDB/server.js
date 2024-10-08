cbe4c77e980ec7e51ab3202ec9c86271f50054c9
Ahmed Hany <ah7608867@gmail.com>
2024-10-08 21:39:08 +0300

    DB connection

--- 03-Express/01-Express_mongoDB/server.js
+++ 03-Express/01-Express_mongoDB/server.js
@@ -1,7 +1,14 @@
 const dotenv = require('dotenv');
-
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
+const database = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+console.log(database);
+mongoose.connect(database, {})
+    .then(connection => {
+        console.log(connection.connections);
+        console.log('DB connection successful');
+    });
 const app = require('./app');
 // start express app
 const PORT = process.env.PORT;
