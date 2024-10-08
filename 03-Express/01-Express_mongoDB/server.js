6b18180fe418a3a08d8f2f03565879595c6ad014
Ahmed Hany <ah7608867@gmail.com>
2024-10-08 21:19:39 +0300

    configrations

--- 03-Express/01-Express_mongoDB/server.js
+++ 03-Express/01-Express_mongoDB/server.js
@@ -1,7 +1,10 @@
-const app = require('./app');
+const dotenv = require('dotenv');
 
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
 // start express app
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`You are listening to port ${PORT}`);
     console.log('api url: 127.0.0.1:3000')
