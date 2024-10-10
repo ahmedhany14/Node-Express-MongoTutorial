bc467ab7d2ea2b6514761ee74734f6979f6916b7
Ahmed Hany <ah7608867@gmail.com>
2024-10-10 15:05:42 +0300

    Data base connection

--- 03-Express/01-Express_mongoDB/server.js
+++ 03-Express/01-Express_mongoDB/server.js
@@ -1,14 +1,45 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './Config.env' });
 
-const database = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-console.log(database);
+const database = process.env.DATABASE
+
 mongoose.connect(database, {})
     .then(connection => {
-        console.log(connection.connections);
         console.log('DB connection successful');
     });
+
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: [true, "you should add the name"]
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, "you should add the price"]
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+
+const new_tour = new Tour({
+    name: 'test3',
+    rating: 5,
+});
+
+new_tour.save().then((docs) => {
+    console.log(docs);
+}).catch(err => {
+    console.log('Error')
+})
+
 const app = require('./app');
 // start express app
 const PORT = process.env.PORT;
