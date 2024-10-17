a1332c995ddfe333eb4771108b6421543bbfd9fa
Ahmed Hany <ah7608867@gmail.com>
2024-10-17 19:13:12 +0300

    development and production error handling

--- 03-Express/controller/errorController.js
+++ 03-Express/controller/errorController.js
@@ -1,10 +1,39 @@
-module.exports = (err, req, res, next) => {
 
-    err.statusCode = err.statusCode || '500';
-    err.status = err.status || 'error';
-
+const developmentError = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
-        message: err.message
+        message: err.message,
+        error: err,
+        stack: err.stack
     })
+}
+
+const productionError = (err, res) => {
+
+    if (err.operationalErorr) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        })
+    }
+    else {
+        res.status(500).json({
+            states: 'error',
+            message: 'Something went wrong'
+        })
+    }
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack
+    })
+}
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || '500';
+    err.status = err.status || 'error';
+    console.log(process.env.NODE_ENV)
+    if (process.env.NODE_ENV === 'development') developmentError(err, res);
+    else productionError(err, res);
 }
