b442879f64b247e24bf19761147841ff06e796be
Ahmed Hany <ah7608867@gmail.com>
2024-10-17 15:18:59 +0300

    Handel unhandeled routes

--- 03-Express/app.js
+++ 03-Express/app.js
@@ -16,4 +16,11 @@ app.use(morgan('dev'));
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
+// Handel unhandeled routes
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'failed',
+        message: `Can't find ${req.originalUrl} on the server`
+    })
+});
 module.exports = app;
