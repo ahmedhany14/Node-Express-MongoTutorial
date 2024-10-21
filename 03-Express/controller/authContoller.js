c27556fbc70a3b89bd053a6a8ca857d36c827910
Ahmed Hany <ah7608867@gmail.com>
2024-10-21 19:17:17 +0300

    sign in token

--- 03-Express/controller/authContoller.js
+++ 03-Express/controller/authContoller.js
@@ -1,12 +1,18 @@
+const jwt = require('jsonwebtoken');
 const users = require('./../model/userModel')
-const catchAsyncErrors = require('./../Utils/catchError.js')
+const catchAsyncErrors = require('./../Utils/catchError.js');
 
 exports.signUp = catchAsyncErrors(async (request, responce, next) => {
 
     const user = await users.create(request.body);
 
+    const token = jwt.sign({ id: user._id }, process.env.JWT_Secret, {
+        expiresIn: process.env.JWT_expired
+    })
+    
     responce.status(201).json({
         status: '201',
+        token: token,
         message: user
     });
 })
