edf0dd76ad0199134342e756de173b0c8c70c076
Ahmed Hany <ah7608867@gmail.com>
2024-10-27 15:22:07 +0300

    request limiting

--- 03-Express/app.js
+++ 03-Express/app.js
@@ -5,14 +5,22 @@ const AppErrors = require('./Utils/appErros')
 const toursRouter = require('./routes/tourRoutes');
 const usersRouter = require('./routes/userRoutes');
 const ErrorHadeler = require('./controller/errorController')
+const limitRate = require('express-rate-limit')
+
 app.use(express.json()); // middleware
 
 
 // third party middleware
 app.use(morgan('dev'));
 
+const limiter = limitRate({
+    max: 2,
+    windowMs: 60 * 60 * 1000,
+    message: "to many requests"
+})
 
-
+// will make ant ip requests the url in the rout /api, have only 100 request per hour
+app.use('/api', limiter)
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
