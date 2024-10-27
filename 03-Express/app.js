bad32dfec0fb42b6cd181a12c08549d277b095fc
Ahmed Hany <ah7608867@gmail.com>
2024-10-27 15:37:20 +0300

    Setting Security HTTP Headers

--- 03-Express/app.js
+++ 03-Express/app.js
@@ -6,21 +6,26 @@ const toursRouter = require('./routes/tourRoutes');
 const usersRouter = require('./routes/userRoutes');
 const ErrorHadeler = require('./controller/errorController')
 const limitRate = require('express-rate-limit')
+const helmet = require('helmet')
 
-app.use(express.json()); // middleware
 
+app.use(helmet())
+
+// middleware
+app.use(express.json({limit: '10kb'}));
 
 // third party middleware
-app.use(morgan('dev'));
+if (process.env.NODE_ENV == 'development') app.use(morgan('dev'));
 
+// middleware to limit number of requests
 const limiter = limitRate({
     max: 2,
     windowMs: 60 * 60 * 1000,
     message: "to many requests"
 })
-
 // will make ant ip requests the url in the rout /api, have only 100 request per hour
 app.use('/api', limiter)
+
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
