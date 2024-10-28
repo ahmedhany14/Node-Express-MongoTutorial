412a6645d29b542703972ac0bc9812b9a9b30a72
Ahmed Hany <ah7608867@gmail.com>
2024-10-28 20:43:27 +0300

    import real date [with location to deal with]

--- 03-Express/dev-data/data/importingScript.js
+++ 03-Express/dev-data/data/importingScript.js
@@ -2,19 +2,18 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const file_system = require('fs')
 
-dotenv.config({ path: './Config.env' });
+dotenv.config({path: './Config.env'});
 const database = process.env.DATABASE
 
-console.log(database)
-const tours = JSON.parse(file_system.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(file_system.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 
 mongoose.connect(database, {})
     .then(connection => {
         console.log('DB connection successful');
     }).catch(err => {
-        console.log("DataBase connection error");
-    })
+    console.log("DataBase connection error");
+})
 
 // node dev-data/data/importingScript.js to run the script
 console.log(process.argv);
@@ -23,11 +22,9 @@ const Tours = require('./../../model/tourModel')
 
 async function loadata() {
     try {
-
         await Tours.create(tours)
         console.log("Data added succesfully");
-    }
-    catch (err) {
+    } catch (err) {
         console.log('error in adding the data')
         console.log(err.message)
     }
@@ -38,18 +35,19 @@ async function dropdata() {
     try {
         await Tours.deleteMany();
         console.log("Data dropped succesfully");
-    }
-    catch (err) {
+    } catch (err) {
         console.log('error in dropping the data')
         console.log(err.message)
     }
     process.exit();
 }
 
+const arg = process.argv.find(val => val === '--add')
 
-if (process.argv[2] == '--add') {
+if (arg === '--add') {
+    console.log('adding data')
     loadata();
 }
-if (process.argv[2] == '--drop') {
+if (arg === '--drop') {
     dropdata();
 }
