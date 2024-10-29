57d1313a6161bfef77de994b2ffde7f094ec5f5f
Ahmed Hany <ah7608867@gmail.com>
2024-10-30 00:44:35 +0300

    edit reviews schema and make only regular users make a reviews

--- 03-Express/model/reviewsModel.js
+++ 03-Express/model/reviewsModel.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 
 
 const reviewSchema = new mongoose.Schema({
-        rating: {
-            type: Number, max: 5, min: 1, required: [true, 'please provide a rating']
-        }, createdAt: {
-            type: Date, default: Date.now()
-        }, userId: {
-            type: mongoose.Schema.ObjectId, ref: "users", required: [true, 'please provide a user id']
-        }, tourId: {
-            type: mongoose.Schema.ObjectId, ref: 'Tour', required: [true, 'please provide a tour id']
-        }
-    }, // to apply the virtual properties, we need to make it true in the schema
+    review: { type: String, required: [true, 'please provide a review'] },
+    rating: {
+        type: Number, max: 5, min: 1, required: [true, 'please provide a rating']
+    }, createdAt: {
+        type: Date, default: Date.now()
+    }, userId: {
+        type: mongoose.Schema.ObjectId, ref: "users", required: [true, 'please provide a user id']
+    }, tourId: {
+        type: mongoose.Schema.ObjectId, ref: 'Tour', required: [true, 'please provide a tour id']
+    }
+}, // to apply the virtual properties, we need to make it true in the schema
     {
-        toJSON: {virtuals: true}, toObject: {virtuals: true},
+        toJSON: { virtuals: true }, toObject: { virtuals: true },
     });
 
 
--- 03-Express/routes/reviewRouter.js
+++ 03-Express/routes/reviewRouter.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const { writeReview, getReviews } = require('./../controller/reviewsContorl')
-const { protect } = require('./../controller/authContoller')
+const { protect, Permission } = require('./../controller/authContoller')
 const router = express.Router();
 
 
 router.route('/:id')
     .post(protect, writeReview)
-    .get(protect, getReviews)
+    .get(protect, Permission('user'), getReviews)
 
 module.exports = router
