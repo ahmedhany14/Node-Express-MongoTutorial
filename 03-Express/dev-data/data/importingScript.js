5f7cf0ddaf215ff41206cc5c07ae2f61a9b58227
Ahmed Hany <ah7608867@gmail.com>
2024-10-31 15:12:45 +0300

    import data to the database

--- 03-Express/dev-data/data/importingScript.js
+++ 03-Express/dev-data/data/importingScript.js
@@ -1,28 +1,34 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const file_system = require('fs')
+const Tours = require('./../../model/tourModel')
+const Users = require('./../../model/userModel')
+const Reviews = require('./../../model/reviewsModel')
 
-dotenv.config({path: './Config.env'});
+dotenv.config({ path: './Config.env' });
 const database = process.env.DATABASE
 
 const tours = JSON.parse(file_system.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(file_system.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(file_system.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 
 mongoose.connect(database, {})
     .then(connection => {
         console.log('DB connection successful');
     }).catch(err => {
-    console.log("DataBase connection error");
-})
+        console.log("DataBase connection error");
+    })
 
 // node dev-data/data/importingScript.js to run the script
 console.log(process.argv);
 
-const Tours = require('./../../model/tourModel')
-
 async function loadata() {
     try {
         await Tours.create(tours)
+        await Users.create(users, { validateBeforeSave: false });
+        await Reviews.create(reviews)
+
         console.log("Data added succesfully");
     } catch (err) {
         console.log('error in adding the data')
@@ -34,6 +40,9 @@ async function loadata() {
 async function dropdata() {
     try {
         await Tours.deleteMany();
+        await Users.deleteMany();
+        await Reviews.deleteMany();
+
         console.log("Data dropped succesfully");
     } catch (err) {
         console.log('error in dropping the data')
@@ -42,12 +51,7 @@ async function dropdata() {
     process.exit();
 }
 
-const arg = process.argv.find(val => val === '--add')
+const arg = process.argv[2];
 
-if (arg === '--add') {
-    console.log('adding data')
-    loadata();
-}
-if (arg === '--drop') {
-    dropdata();
-}
+if (arg === '--add') loadata();
+if (arg === '--drop') dropdata();
