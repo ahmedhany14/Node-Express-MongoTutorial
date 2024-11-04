a9ada85f009a58def74f57ac077268f9673fa6a2
Ahmed Hany <ah7608867@gmail.com>
2024-11-04 15:52:40 +0200

    success checkout (store data n DB)

--- 03-Express/controller/bokkingController.js
+++ 03-Express/controller/bokkingController.js
@@ -1,34 +1,46 @@
 const catchAsync = require('./../Utils/catchError')
+const AppError = require('./../Utils/appErros')
 const Tour = require('./../model/tourModel')
+const bookings = require('./../model/bookingModel')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 exports.CreateSession = catchAsync(async (request, response, next) => {
     const id = request.params.tourId;
     const tour = await Tour.findById(id);
     // Create a stripe session
+    const dirict_url = `${request.protocol}://${request.get('host')}/api/v1/bookings/susses/?tour=${id}&user=${request.user._id}&price=${tour.price}`
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
-        success_url: `${request.protocol}://${request.get('host')}/api/v1/tours/`,
+        success_url: dirict_url,
         cancel_url: `${request.protocol}://${request.get('host')}/api/v1/tours/`,
         customer_email: request.user.email,
         client_reference_id: id,
-        line_items: [
-            {
-                price_data: {
-                    currency: 'usd',
-                    product_data: {
-                        name: tour.name,
-                    },
-                    unit_amount: tour.price * 100,
-                },
-                quantity: 1,
-            },
-        ],
+        line_items: [{
+            price_data: {
+                currency: 'usd', product_data: {
+                    name: tour.name,
+                }, unit_amount: tour.price * 100,
+            }, quantity: 1,
+        },],
         mode: 'payment'
     });
 
     response.status(200).json({
-        status: 'ok',
-        session
+        status: 'ok', session
     })
+})
+
+exports.successCheckout = catchAsync(async (request, response, next) => {
+    const {tour, user, price} = request.query;
+    if (!tour || !user || !price) return next(new AppError('some data lost, try again', 404));
+
+    const Obj = new bookings({
+        tour: tour, user: user, price: +price, createdAt: Date.now(), paid: true
+    })
+
+    await Obj.save();
+    response.status(200).json({
+        status: "success", bookDetails: Obj
+    })
+
 })
--- 03-Express/routes/bookingRouter.js
+++ 03-Express/routes/bookingRouter.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const {CreateSession} = require('./../controller/bokkingController')
+const {CreateSession, successCheckout} = require('./../controller/bokkingController')
 const {protect} = require('./../controller/authContoller')
 const router = express.Router();
 
 router.route('/check-out/:tourId')
     .get(protect, CreateSession)
+
+router.route('/susses')
+    .get(successCheckout)
 
 module.exports = router;
